c5e04bb8c5d57a842cab90c34dba7d8709525dc8
Kjartan Ólafur Gunnarsson <kog36@hi.is>
2024-04-04 13:47:22 +0000

    Hlutir

--- src/main/java/hi/verkefni/vidmot/ForsidaController.java
+++ src/main/java/hi/verkefni/vidmot/ForsidaController.java
@@ -5,7 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 /**
@@ -19,9 +19,23 @@ public class ForsidaController {
 
 
     private static final String[] lagaListi= {"Lag 1", "lag 2", "Lag 3",  "lag 4"};
+    public Label fxEStig;
     @FXML
     private ComboBox<String> tonlist;
+    @FXML
+    private ToggleGroup toggleGroup1;
+    private String[] eStig = {"Létt","Miðlungs", "Erfitt"};
+
     public void initialize(){
+
+        toggleGroup1.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null){
+                RadioButton selectedMenuItem = (RadioButton) newValue;
+                int difficulty = Integer.parseInt(selectedMenuItem.getId());
+                fxEStig.setText(eStig[difficulty-1]);
+
+            }
+        });
         frumstillaLog();
     }
     public void frumstillaLog(){
@@ -29,12 +43,15 @@ public class ForsidaController {
         tonlist.setItems(lagaNofn);
     }
 
+
+
     /**
-         * Event handler fyrir takkann á forsíðunni. Skiptir um senu og hefur leik.
-         * @param event
-         * @throws IOException
-         */
-        public void fxOnByrjaTakki(ActionEvent event) throws IOException {
-            ViewSwitcher.switchTo(View.LEIKUR);
-        }
+     * Event handler fyrir takkann á forsíðunni. Skiptir um senu og hefur leik.
+     * @param event
+     * @throws IOException
+     */
+    public void fxOnByrjaTakki(ActionEvent event) throws IOException {
+         ViewSwitcher.switchTo(View.LEIKUR);
+    }
+
 }
